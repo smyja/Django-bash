805a73f04265634afc94ce0fe1c579ffc149a424
Smyja <akposlive59@gmail.com>
2022-06-20 18:16:41 +0100

    Revert "Edit scripts and README"

--- a.sh
+++ a.sh
@@ -12,20 +12,16 @@ blue=`tput setaf 4`
 magenta=`tput setaf 5`
 cyan=`tput setaf 6`
 reset= `tput sgr0`
-
 curl https://raw.githubusercontent.com/github/gitignore/main/Python.gitignore > .gitignore
-
-pip install django===3.2.13
-
-#Django project setup
+pip install django
 echo "${green}Installing Django${reset}"
-read -p "${blue}Enter a Project name${reset}: " project_name
+echo "${blue}Enter a Project name${reset}"
+read project_name
 echo "${cyan}Creating Django project${reset}"
 django-admin startproject $project_name .
 echo "Project created"
-
-#Django app creation
-read -p "${green}Enter an App name${reset}: " app_name
+echo "${green}Creating a new Django app,Enter an app name${reset}"
+read app_name
 python manage.py startapp $app_name
 echo "${magenta}Creating a new Django app${reset}"
 
@@ -46,7 +42,6 @@ echo "from django.shortcuts import render" >> $app_name/views.py
 echo "def index(request):" >> $app_name/views.py
 echo "    return render(request, 'index.html')" >> $app_name/views.py
 echo "${green}View created${reset}"
-
 #add app to urls.py
 echo "${green}Adding app to urls.py......${reset}"
 touch $app_name/urls.py
